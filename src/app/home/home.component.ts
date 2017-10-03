8b53d5cbee3044bf45507b817e6f03375eb37b86
danielcb29 <danielc.web@gmail.com>
2017-10-02 21:35:34 -0500

    Issue #8: Hide social media when footer is toggle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { trigger, transition, animate , style} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-
+  animations: [
+    trigger('showMedia', [
+      transition(':leave', animate('100ms ease-out', style({transform: 'translateX(100%)', opacity: 0}))),
+    ]),
+  ]
 })
 export class HomeComponent {
 
-  
+  showMedia = true;
+
+  onFooterToogle(infoState: string) {
+    this.showMedia = infoState === 'hide' ? true : false;
+  }
 
 }
